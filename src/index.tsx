ffad4bd19c0bf7d76ba42d3afafdbc559b824bc0
vysohio71 <yoshio.victor@hotmail.com>
2024-09-08 19:37:27 -0300

    feat: add auth persistence

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import { Provider } from "react-redux";
-import store from "./store/store";
+import { store, persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -16,7 +17,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
