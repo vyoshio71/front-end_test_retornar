231c408a8275f6c6cc66c6d8e2bcc784afd6925c
vysohio71 <yoshio.victor@hotmail.com>
2024-09-08 23:08:26 -0300

    feat: insert OrderCard component in orders view

--- src/components/DropDownMenu/dropDownMenu.tsx
+++ src/components/DropDownMenu/dropDownMenu.tsx
@@ -3,6 +3,8 @@ import { useDispatch } from "react-redux";
 import { logout } from "../../redux/authSlice";
 
 import "../../css/components/dropdownMenu.css";
+
+import OrderCard from "../OrderCard/orderCard";
 import Modal from "../Modal/modal";
 
 import { getAllAcai } from "../../services/acaiServices";
@@ -13,13 +15,33 @@ import { persistor } from "../../store/store";
 const DropdownMenu: React.FC<IDropdownMenu> = ({ isOpen, onClose }) => {
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [acaiList, setAcaiList] = useState<IAcai[]>([]);
+  const [deliveredAcaiList, setDeliveredAcaiList] = useState<IAcai[]>([]);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchAcai = async () => {
       try {
         const data = await getAllAcai();
-        setAcaiList(data);
+        const currentTime = new Date();
+
+        const activeOrders = data.filter((acai) => {
+          const estimatedDeliveryTime = new Date(acai.created_at);
+          estimatedDeliveryTime.setMinutes(
+            estimatedDeliveryTime.getMinutes() + acai.estimate
+          );
+          return estimatedDeliveryTime > currentTime;
+        });
+
+        const deliveredOrders = data.filter((acai) => {
+          const estimatedDeliveryTime = new Date(acai.created_at);
+          estimatedDeliveryTime.setMinutes(
+            estimatedDeliveryTime.getMinutes() + acai.estimate
+          );
+          return estimatedDeliveryTime <= currentTime;
+        });
+
+        setAcaiList(activeOrders);
+        setDeliveredAcaiList(deliveredOrders);
       } catch (error) {
         console.error("Erro ao buscar a lista de açaí:", error);
       }
@@ -30,7 +52,7 @@ const DropdownMenu: React.FC<IDropdownMenu> = ({ isOpen, onClose }) => {
 
   const handleLogout = () => {
     dispatch(logout());
-    persistor.purge()
+    persistor.purge();
     onClose();
   };
 
@@ -49,30 +71,12 @@ const DropdownMenu: React.FC<IDropdownMenu> = ({ isOpen, onClose }) => {
           <div className="acai-list">
             {acaiList &&
               acaiList.map((acai) => (
-                <div key={acai.id} className="acai-item">
-                  <p>
-                    <strong>Tamanho:</strong> {acai.size}
-                  </p>
-                  <p>
-                    <strong>Fruta:</strong> {acai.fruit}
-                  </p>
-                  <p>
-                    <strong>Acompanhamentos:</strong>{" "}
-                    {acai.side_dishes.join(", ")}
-                  </p>
-                  <p>
-                    <strong>Preço Total:</strong> R${acai.total_price},00
-                  </p>
-                  <p>
-                    <strong>Estimativa:</strong> {acai.estimate} minutos
-                  </p>
-                  <p>
-                    <strong>Total de Itens:</strong> R${acai.total_price},00
-                  </p>
-                  <p>
-                    <strong>Quantidade:</strong> {acai.amount}
-                  </p>
-                </div>
+                <OrderCard key={acai.id} acai={acai} isDelivered={false} />
+              ))}
+
+            {deliveredAcaiList &&
+              deliveredAcaiList.map((acai) => (
+                <OrderCard key={acai.id} acai={acai} isDelivered={true} />
               ))}
           </div>
         </div>
